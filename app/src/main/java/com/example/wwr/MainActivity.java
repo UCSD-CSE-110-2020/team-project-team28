41a3c1630a7a1fdcd5d602f7fe0ade097130fc80
Yong Chung <ywc005@ucsd.edu>
2020-02-08 12:23:26 -0800

    Skeleton code for main page

--- app/src/main/java/com/example/wwr/MainActivity.java
+++ app/src/main/java/com/example/wwr/MainActivity.java
@@ -3,9 +3,6 @@ package com.example.wwr;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
@@ -23,7 +20,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        Button button = (Button) findViewById(R.id.button);
+        Button button = (Button) findViewById(R.id.dailyRoutesButton);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
