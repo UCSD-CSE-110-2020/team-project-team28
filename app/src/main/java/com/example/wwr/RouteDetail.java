b4795a183a3395fd0fcc79d4f3367741c4d155a4
Wonsuk Jang <Tim@MacBook-Pro-139.local>
2020-02-08 13:41:12 -0800

    Linked some parts

--- app/src/main/java/com/example/wwr/RouteDetail.java
+++ app/src/main/java/com/example/wwr/RouteDetail.java
@@ -2,7 +2,10 @@ package com.example.wwr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -32,6 +35,15 @@ public class RouteDetail extends AppCompatActivity {
         steps.setText(getIntent().getStringExtra("steps"));
         distance.setText(getIntent().getStringExtra("distance"));
         note.setText(getIntent().getStringExtra("note"));
+
+        Button routeInfo = (Button) findViewById(R.id.route_info_start_button);
+        routeInfo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), WalkScreenActivity.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/wwr/RoutesActivity.java
@@ -0,0 +1,93 @@
+package com.example.wwr;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.content.SharedPreferences;
+
+public class RoutesActivity extends AppCompatActivity {
+    private final String EMPTY_STRING = "";
+
+    RadioGroup flatGroup, loopGroup, streetGroup, surfaceGroup, difficultyGroup;
+    RadioButton flatButton, loopButton, streetButton, surfaceButton, difficultyButton;
+    EditText routeName, startLocation, notes;
+    Button okButton;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_routes);
+
+        //initialize all group variables
+        flatGroup = findViewById(R.id.groupFlat);
+        loopGroup = findViewById(R.id.groupLoop);
+        streetGroup = findViewById(R.id.groupStreet);
+        surfaceGroup = findViewById(R.id.groupSurface);
+        difficultyGroup = findViewById(R.id.groupDifficulty);
+        routeName = findViewById(R.id.routeNamePage);
+        startLocation = findViewById(R.id.startLocationName);
+        notes = findViewById(R.id.routeNotes);
+
+        routeName.setText(EMPTY_STRING);
+        startLocation.setText(EMPTY_STRING);
+        notes.setText(EMPTY_STRING);
+
+    } // end onCreate()
+
+    public void pressOK (View view) {
+        // grab radio button selections
+        int radioId = flatGroup.getCheckedRadioButtonId();
+        flatButton = findViewById(radioId);
+
+        radioId = loopGroup.getCheckedRadioButtonId();
+        loopButton = findViewById(radioId);
+
+        radioId = streetGroup.getCheckedRadioButtonId();
+        streetButton = findViewById(radioId);
+
+        radioId = surfaceGroup.getCheckedRadioButtonId();
+        surfaceButton = findViewById(radioId);
+
+        radioId = difficultyGroup.getCheckedRadioButtonId();
+        difficultyButton = findViewById(radioId);
+
+        // grab user inputted text
+        routeName = (EditText) findViewById(R.id.routeNamePage);
+        startLocation = (EditText) findViewById(R.id.startLocationName);
+        notes = (EditText) findViewById(R.id.routeNotes);
+
+        // now save values
+        SharedPreferences userPref = getSharedPreferences(routeName.getText().toString() + "_info", MODE_PRIVATE);
+        SharedPreferences.Editor editor = userPref.edit();
+
+        // set info
+        editor.putString("routeName", routeName.getText().toString());
+        editor.putString("startLocation", startLocation.getText().toString());
+        editor.putString("flatOrHilly", flatButton.getText().toString());
+        editor.putString("loopOrOut", loopButton.getText().toString());
+        editor.putString("streetOrTrail", streetButton.getText().toString());
+        editor.putString("surface", surfaceButton.getText().toString());
+        editor.putString("difficulty", difficultyButton.getText().toString());
+        editor.putString("notes", notes.getText().toString());
+
+
+        // apply changes
+        editor.apply();
+
+        RouteScreen.addToRouteList(routeName.getText().toString(),
+                startLocation.getText().toString(), 0, 0, 0,
+                notes.getText().toString(), false);
+
+        RouteScreen.notifyInsert();
+
+        finish();
+    }
+
+} // end RoutesActivity
