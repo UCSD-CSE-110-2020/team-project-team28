71812aacdfe9975192158a38d2c58f64959b64c0
Wonsuk Jang <Tim@wireless-169-228-109-133.ucsd.edu>
2020-02-15 16:03:06 -0800

    #21,22,23: Completed the following tasks

--- app/src/main/java/com/example/wwr/RouteDetail.java
+++ app/src/main/java/com/example/wwr/RouteDetail.java
@@ -7,11 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
-
-import java.util.ArrayList;
 
 public class RouteDetail extends AppCompatActivity {
     TextView name;
