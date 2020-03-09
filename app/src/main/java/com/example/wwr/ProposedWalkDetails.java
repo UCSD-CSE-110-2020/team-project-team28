c749ac11805d2946eb9f6fc9164f24a03499ebca
Sarah Ekaireb <sekaireb@ucsd.edu>
2020-03-09 15:09:29 -0700

    save propose route accept/decline status for users

--- app/src/main/java/com/example/wwr/ProposedWalkDetails.java
+++ app/src/main/java/com/example/wwr/ProposedWalkDetails.java
@@ -1,14 +1,88 @@
 package com.example.wwr;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProposedWalkDetails extends AppCompatActivity {
+    private static final String TAG = "ProposedWalkDetails";
+    RadioGroup statusGroup;
+    String status;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_proposed_walk_details);
+
+        statusGroup = findViewById(R.id.groupStatus);
+
+
+        Button exit = findViewById(R.id.proposed_walk_exit_button);
+        exit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RadioButton checked = findViewById(statusGroup.getCheckedRadioButtonId());
+                status = checked.getText().toString();
+                addUserStatusToFirebase();
+                finish();
+            }
+        });
+    }
+
+
+    public void addUserStatusToFirebase() {
+        SharedPreferences sharedPreferences = getSharedPreferences("prefs", Context.MODE_PRIVATE);
+        String userName = sharedPreferences.getString("userName", "Test");
+
+        Map<String, Object> users = new HashMap<>();
+        users.put(userName, status);
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("team").document("status")
+                .update(users)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d(TAG, "DocumentSnapshot successfully written!");
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        db.collection("team").document("status")
+                                .set(users)
+                                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                    @Override
+                                    public void onSuccess(Void aVoid) {
+                                        Log.d(TAG, "DocumentSnapshot successfully created!");
+                                    }
+                                })
+                                .addOnFailureListener(new OnFailureListener() {
+                                    @Override
+                                    public void onFailure(@NonNull Exception e) {
+                                        Log.w(TAG, "Error writing document", e);
+                                        Toast.makeText(getApplicationContext(), "Upload failed!",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                    }
+                });
     }
 }
