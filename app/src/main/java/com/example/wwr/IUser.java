ce078ddef84c017c94c4015fe914e7891213f77c
Alan Willey <awilley17@gmail.com>
2020-03-12 15:44:02 -0700

    added strategy pattern for class

---
+++ app/src/main/java/com/example/wwr/IUser.java
@@ -0,0 +1,22 @@
+package com.example.wwr;
+
+
+/*
+ * A user should have the ability to do all the following things and maintain all the following
+ * data points, maintains SRP and follows our design principles, allows functionality of user
+ * to be in just one file
+ */
+interface IUser {
+    // ensures user can always get their own step count
+    long getDailySteps();
+    void setDailySteps(long dailySteps);
+    void saveCurrentSteps();
+    void increaseDailyMockSteps();
+    long getLastIntentSteps();
+
+    // ensures user can get distance and walk time of routes
+    double getDailyMiles();
+    void setLastIntentionalWalkTime(long time);
+    long getLastIntentionalTime();
+    double getLastIntentMiles();
+}
